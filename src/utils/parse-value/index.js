0a5eb967f873f96b9688f96486e46d5bd9bd064f
David Philip Gösenbauer <david.goesenbauer@gmail.com>
2020-06-09 11:30:38 +0200

    Revert "ADD recursion depth hook for value validation to avoid lifecycle hook scripts to exceed node callstack size"
    This reverts commit 3faac7e736f38d273f1689283a4f0f5e03243e20.

--- src/utils/parse-value/index.js
+++ src/utils/parse-value/index.js
@@ -3,8 +3,6 @@
 import { buildPathFromReference } from '../path';
 import DocumentReference from '../../firebase/firestore/document-reference';
 
-const MAX_RECURSION_DEPTH = 2;
-
 export function isObject(value) {
   return Object.prototype.toString.call(value) === '[object Object]';
 }
@@ -27,9 +25,8 @@ function validateValue(value, option) {
   }
 
   if (isObject(value)) {
-    const depth = option.depth ? option.depth + 1 : 1;
-    if (option.isInObject && depth > MAX_RECURSION_DEPTH) return;
-    const newOption = Object.assign({}, option, { isInObject: true, depth });
+    const newOption = Object.assign({}, option, { isInObject: true });
+
     Object.keys(value).forEach(key => validateValue(value[key], newOption));
   }
 
